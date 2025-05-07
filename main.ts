e399e3ef9b74a257f8d4ee7a90fbe3656684609b
raph2leretour <tisserandraph@gmail.com>
2025-05-07 12:58:11 +0200

    new start

--- main.ts
+++ main.ts
@@ -1,124 +1,34 @@
-class DrawingApp {
+const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d")!;
 
-	private canvas: HTMLCanvasElement;
-	private context: CanvasRenderingContext2D;
-	private paint: boolean;
+let ballX = canvas.width / 2;
+let ballY = canvas.height / 2;
+let ballSpeedX = 5;
+let ballSpeedY = 4;
 
-	private clickX: number[] = [];
-	private clickY: number[] = [];
-	private clickDrag: boolean[] = [];
+function draw() {
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	constructor() {
-		let canvas = document.getElementById('canvas') as HTMLCanvasElement;
-		let context = canvas.getContext("2d");
+	ctx.fillStyle = "white";
+	ctx.beginPath();
+	ctx.arc(ballX, ballY, 10, 0, Math.PI * 2);
+	ctx.fill();
+}
 
-		context.lineCap = 'round';
-		context.lineJoin = 'round';
-		context.strokeStyle = 'black';
-		context.lineWidth = 1;
+function update() {
+	ballX += ballSpeedX;
+	ballY += ballSpeedY;
 
-		this.canvas = canvas;
-		this.context = context;
-
-		this.redraw();
-		this.createUserEvents();
-	}
-
-	private createUserEvents() {
-		let canvas = this.canvas();
-
-		canvas.addEventListener("mousedown", this.pressEventHandler);
-		canvas.addEventListener("mousemove", this.dragEventHandler);
-		canvas.addEventListener("mouseup", this.releaseEventHandler);
-		canvas.addEventListener("mouseout", this.cancelEventHandler);
-
-		canvas.addEventListener("touchstart", this.pressEventHandler);
-		canvas.addEventListener("touchmove", this.dragEventHandler);
-		canvas.addEventListener("touchend", this.releaseEventHandler);
-		canvas.addEventListener("touchcancel", this.cancelEventHandler);
-
-		document.getElementById('clear')
-			.addEventListener("click", this.clearEventHandler);
-	}
-
-	private redraw() {
-		let clickX = this.clickX;
-		let context = this.context;
-		let clickDrag = this.clickDrag;
-		let clickY = this.clickY;
-
-		for (let i = 0; i < clickX.length; ++i) {
-			context.beginPath();
-			if (clickDrag[i] && i) {
-				context.moveTo(clickX[i - 1], clickY[i - 1]);
-			} else {
-				context.moveTo(clickX[i] - 1, clickY[i]);
-			}
-
-			context.lineTo(clickX[i], clickY[i]);
-			context.stroke();
-		}
-		context.closePath()();
-	}
-
-	private addClick(x: number, y: number, dragging: boolean) {
-		this.clickX.push(x);
-		this.clickY.push(y);
-		this.clickDrag.push(dragging);
-	}
-
-	private clearCanvas() {
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		this.clickX = [];
-		this.clickY = [];
-		this.clickDrag = [];
-	}
-
-	private clearEventHandler = () => {
-		this.clearCanvas();
-	}
-
-	private releaseEventHandler = () => {
-		this.paint = false;
-		this.redraw();
-	}
-
-	private cancelEventHandler = () => {
-		this.paint = false;
-	}
-
-	private pressEventHandler = (e: MouseEvent | TouchEvent) => {
-		let mouseX = (e as TouchEvent).changedTouches ?
-			(e as TouchEvent).changedTouches[0].pageX :
-			(e as MouseEvent).pageX;
-		let mouseY = (e as TouchEvent).changedTouches ?
-			(e as TouchEvent).changedTouches[0].pageY :
-			(e as MouseEvent).pageY;
-		mouseX -= this.canvas.offsetLeft;
-		mouseY -= this.canvas.offsetTop;
-
-		this.paint = true;
-		this.addClick(mouseX, mouseY, false);
-		this.redraw();
-	}
-
-	private dragEventHandler = (e: MouseEvent | TouchEvent) => {
-		let mouseX = (e as TouchEvent).changedTouches ?
-			(e as TouchEvent).changedTouches[0].pageX :
-			(e as MouseEvent).pageX;
-		let mouseY = (e as TouchEvent).changedTouches ?
-			(e as TouchEvent).changedTouches[0].pageY :
-			(e as MouseEvent).pageY;
-		mouseX -= this.canvas.offsetLeft;
-		mouseY -= this.canvas.offsetTop;
-
-		if (this.paint) {
-			this.addClick(mouseX, mouseY, true);
-			this.redraw();
-		}
-
-		e.preventDefault();
+	if (ballY <= 0 || ballY >= canvas.height) {
+		ballSpeedY = -ballSpeedY;
 	}
 }
 
-new DrawingApp();
+function gameLoop() {
+	update();
+	draw();
+	requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
